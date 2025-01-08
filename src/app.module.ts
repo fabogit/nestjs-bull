e02c41b5fc78bf7e8d45f9cb3f092d9086e9e089
fabo <fabo.dev.email@gmail.com>
2025-01-08 19:08:52 +0100

    #1
    bull initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
+import { TRANSCODE_QUEUE } from './bull/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: TRANSCODE_QUEUE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { TRANSCODE_QUEUE } from './bull/constants';
+import { Queue } from 'bull';
+import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectQueue(TRANSCODE_QUEUE) private readonly trasnscodeQueue: Queue,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async transcode() {
+    const data = {
+      fileName: './file.mp3',
+      // ...
+    };
+    await this.trasnscodeQueue.add(data, {});
   }
 }
